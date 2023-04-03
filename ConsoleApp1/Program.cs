01ed3dfaf602912937333599d1a921b6cf38ec5d
Raian Pereira <raianpereiraa@gmail.com>
2023-04-03 09:49:07 -0300

    Version 1.0

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleApp1;
+using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Curso
+{
+    class Exercicio
+    {
+        static void Main(string[] args)
+        {
+
+            Usuario titular;
+
+            Console.Write("Digite o número da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome do titular: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n) ");
+            char resp = char.Parse(Console.ReadLine());
+
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Digite o valor do deposito inicial: ");
+                double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                titular = new Usuario(conta, nome, deposito);
+            }
+            else
+            {
+                titular = new Usuario(conta, nome);
+                
+            }
+
+            Console.WriteLine("Dados da Conta:");
+            Console.WriteLine(titular);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite um valor pra deposito");
+            double quantia = double.Parse(Console.ReadLine());
+            titular.Deposito(quantia);
+            Console.WriteLine(titular);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite um valor pra saque");
+            quantia = double.Parse(Console.ReadLine());
+            titular.Saque(quantia);
+            Console.WriteLine(titular);
+        }
+    }
+}
---
+++ ConsoleApp1/Usuario.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using System.Security.Cryptography;
+
+namespace ConsoleApp1
+{
+    internal class Usuario
+    {
+        public int Conta { get; private set; }
+        private string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+
+
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.00; 
+        }
+
+
+        public void Deposito (double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public Usuario(int conta, string nome)
+        {
+            Conta = conta;
+            Nome = nome;
+        }       
+
+        public Usuario(int conta, string nome, double depositoInicial) : this(conta, nome)
+        {
+            Deposito(depositoInicial);  
+        }
+
+
+        public override string ToString()
+        {
+            return "Dados da Conta: "
+                + Conta 
+                +" , Titular: "
+                + Nome
+                +" Saldo: "
+                +Saldo;
+        }
+
+    }
+}
